bf08fc612b09766319fc9432d4c690d464f86353
Arthur Pedroti <arthurpedroti@gmail.com>
2022-02-02 17:34:41 -0300

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/arthurpedroti/go-project/infra/kafka"
+	repository2 "github.com/arthurpedroti/go-project/infra/repository"
+	usecase2 "github.com/arthurpedroti/go-project/usecase"
+	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func main() {
+	db, err := sql.Open("mysql", "root:root@tcp(mysql:3306)/fullcycle")
+	if err != nil {
+		log.Fatalln(err)
+	}
+	repository := repository2.CourseMySQLRepository{Db: db}
+	usecase := usecase2.CreateCourse{Repository: repository}
+
+	var msgChan = make(chan *ckafka.Message)
+	configMapConsumer := &ckafka.ConfigMap{
+		"bootstrap.servers": "kafka:9094",
+		"group.id":          "appgo",
+	}
+	topics := []string{"courses"}
+	consumer := kafka.NewConsumer(configMapConsumer, topics)
+
+	go consumer.Consume(msgChan)
+
+	for msg := range msgChan {
+		var input usecase2.CreateCourseInputDto
+		json.Unmarshal(msg.Value, &input)
+		output, err := usecase.Execute(input)
+		if err != nil {
+			fmt.Println("Error: ", err)
+		} else {
+			fmt.Println(output)
+		}
+	}
+}
+
+// {"name":"Go","description":"Go is a programming language","status":"pending"}
